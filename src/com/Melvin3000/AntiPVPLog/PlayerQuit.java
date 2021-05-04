95736d2b86fad67a3ea0434085a50e4b737ca631
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-05-04 17:14:52 -0400

    Update PlayerQuit.java

--- src/com/Melvin3000/AntiPVPLog/PlayerQuit.java
+++ src/com/Melvin3000/AntiPVPLog/PlayerQuit.java
@@ -1,5 +1,9 @@
 package com.Melvin3000.AntiPVPLog;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,11 +12,22 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit implements Listener {
 
+	
+	static Player p = null;
+	
+	
+	//Instead of checking if we should spawn a skeleton, we first need a list of all players
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
-
-		Player player = event.getPlayer();
-
+		p = event.getPlayer();
+		BungeeMessenger.PlayerList();
+	}
+	
+	//After we get a list of all players, check if we can spawn a skeleton
+	public static void newSkeleton() {
+		
+		Player player = p;
+		
 		if (!AntiPVPLog.activeWorlds.contains(player.getWorld().getName())) {
 			return;
 		}
@@ -26,16 +41,19 @@ public class PlayerQuit implements Listener {
 			return;
 		}
 
-		if (JailPlugin.isPlayerJailed(player.getUniqueId())) {
-			return;
-		}
-
 		if (player.getGameMode() != GameMode.SURVIVAL) {
 			return;
 		}
-
-		else {
-			new PVPLoggedPlayer(event.getPlayer());
+		
+		//Check if player is still online
+		List<String> playerList = new ArrayList<>(Arrays.asList(BungeeMessenger.playerList));
+		
+		if (playerList.contains(player.getName())) {
+			return;
 		}
+				
+		//Spawn Skeleton
+		new PVPLoggedPlayer(player);
 	}
+	
 }
