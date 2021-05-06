46e069d1294df6d0a3954bf95b6351481f28bb9a
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-05-06 05:35:34 -0400

    Update BungeeMessenger.java

--- src/com/Melvin3000/AntiPVPLog/BungeeMessenger.java
+++ src/com/Melvin3000/AntiPVPLog/BungeeMessenger.java
@@ -1,10 +1,11 @@
 package com.Melvin3000.AntiPVPLog;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
@@ -15,15 +16,29 @@ public class BungeeMessenger implements PluginMessageListener{
 	//Receive player list
 	 @Override
 	    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-	        if (channel.equals("BungeeCord")) {
+	        
+		 	if (channel.equals("BungeeCord")) {
 	        	try {
-		            ByteArrayDataInput in = ByteStreams.newDataInput(message);
+		            //ByteArrayDataInput in = ByteStreams.newDataInput(message);
+		            DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
 		            String subchannel = in.readUTF();
 		            if (subchannel.equals("PlayerList")) {
 		            	//Below part might need to be here
 		                String server = in.readUTF();
 		                playerList = in.readUTF().split(", ");
 		                
+		                PlayerQuit.newSkeleton();
+		            }
+		            
+		            if (subchannel.equals("ForwardChannel")) {
+		            	short len = in.readShort();
+		                byte[] data = new byte[len];
+		                in.readFully(data);
+
+		                //bytearray to string
+		                String s = new String(data);
+
+		                System.out.println(s);
 		            }
 	        	} catch (Exception e) {
 	        		e.printStackTrace();
@@ -40,4 +55,5 @@ public class BungeeMessenger implements PluginMessageListener{
 			  Bukkit.getServer().sendPluginMessage(AntiPVPLog.instance, "BungeeCord", out.toByteArray());
 		}
 			
+	
 }
